6d2389f34e95371fca1a0a041705b26bc4e1d7e2
MARIO HUARACHA <1822894@cmontmorency.qc.ca>
2024-09-26 11:56:05 -0400

    deploiement

--- Backend/app.js
+++ Backend/app.js
@@ -8,39 +8,39 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-  });
-
-  app.use("/offres", offresRoutes);
-  app.use("/users", usersRoutes);
-
-  app.use((req, res, next) => {
-    const error = new Error("Route non trouvée");
-    error.code = 404;
-    next(error);
-  });
-  app.use(errorHandler);
-
-  const port = 5000;
-  const uri = "mongodb+srv://2265470:i8Fm7j4giBKmbfgm@jobbang-bd.ybvsp.mongodb.net/?retryWrites=true&w=majority&appName=jobbang-bd";
-
-  //const uri = "mongodb://localhost:27017/job";
-
-mongoose
-.connect(uri)
-.then(() => {
-  app.listen(port);
-  console.log(`Connexion à la BD [${uri}] sur le port ${port} réussie.`);
-})
-.catch((e) => {
-  console.log(`Connexion à la BD [${uri} sur le port ${port} échouée.]`);
-  console.log(e);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
 
-  
+app.use("/offres", offresRoutes);
+app.use("/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Route non trouvée");
+  error.code = 404;
+  next(error);
+});
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://2265470:i8Fm7j4giBKmbfgm@jobbang-bd.ybvsp.mongodb.net/?retryWrites=true&w=majority&appName=jobbang-bd";
+
+//const uri = "mongodb://localhost:27017/job";
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(port);
+    console.log(`Connexion à la BD [${uri}] sur le port ${port} réussie.`);
+  })
+  .catch((e) => {
+    console.log(`Connexion à la BD [${uri} sur le port ${port} échouée.]`);
+    console.log(e);
+  });
--- Frontend/src/components/inscription/Inscription.jsx
+++ Frontend/src/components/inscription/Inscription.jsx
@@ -1,48 +1,48 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { AuthContext } from "../context/AuthContext";
 
 export default function Inscription(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [type, setType] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("");
+  const navigate = useNavigate();
 
-    const { user, token } = useAuthContext();
-    const auth = useContext(AuthContext);
-    const [error, SetError] = useState(null);
+  const { user, token } = useAuthContext();
+  const auth = useContext(AuthContext);
+  const [error, SetError] = useState(null);
 
-
-    async function authSubmitHandler(event) {
-        event.preventDefault();
-        const inputs = new FormData(event.target);
-        const data = Object.fromEntries(inputs.entries());
-        console.log("data ", data);
-        event.target.reset();
-        try {
-            const response = await fetch("http://localhost:5000/users/register", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data),
-            });
-
-            const responseData = await response.json();
-            console.log("2", responseData);
-            auth.login(responseData.user.id, responseData.token);
-            if (responseData.user.id !== undefined) {
-                navigate("/offres");
-            }
-            console.log("data1 ", responseData);
-        } catch (err) {
-            SetError(err.message || "une erreur");
-            console.log(err);
+  async function authSubmitHandler(event) {
+    event.preventDefault();
+    const inputs = new FormData(event.target);
+    const data = Object.fromEntries(inputs.entries());
+    console.log("data ", data);
+    event.target.reset();
+    try {
+      const response = await fetch(
+        process.send.REACT_APP_BACKEND_URL + "messages/",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
         }
+      );
+
+      const responseData = await response.json();
+      console.log("2", responseData);
+      auth.login(responseData.user.id, responseData.token);
+      if (responseData.user.id !== undefined) {
+        navigate("/offres");
+      }
+      console.log("data1 ", responseData);
+    } catch (err) {
+      SetError(err.message || "une erreur");
+      console.log(err);
     }
+  }
 
-
-
-    /*
+  /*
     const handleRegister = (e) => {
         e.preventDefault();
 
@@ -63,22 +63,40 @@ export default function Inscription(props) {
         }
     }; */
 
-    return (
-        <form onSubmit={authSubmitHandler}>
-            <div>
-                <label>Email :</label>
-                <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </div>
-            <div>
-                <label>Mot de passe :</label>
-                <input type="password" name="mdp" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            </div>
-            <div>
-                <label>Type :</label>
-                <input type="type" name="type" value={props.type} onChange={(e) => setType(e.target.value)} required />
-            </div>
+  return (
+    <form onSubmit={authSubmitHandler}>
+      <div>
+        <label>Email :</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Mot de passe :</label>
+        <input
+          type="password"
+          name="mdp"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Type :</label>
+        <input
+          type="type"
+          name="type"
+          value={props.type}
+          onChange={(e) => setType(e.target.value)}
+          required
+        />
+      </div>
 
-            <button type="submit">Inscription</button>
-        </form>
-    );
+      <button type="submit">Inscription</button>
+    </form>
+  );
 }
